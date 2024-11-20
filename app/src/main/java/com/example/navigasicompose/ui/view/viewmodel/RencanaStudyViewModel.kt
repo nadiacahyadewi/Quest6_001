9ef5b4ecf3646a34b4647a4303094173919c2d98
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2024-11-20 22:19:44 +0700

    Menambahkan ViewModel RencanaStudyViewModel dengan MutableStateFlow untuk menyimpan status RencanaStudi

--- app/src/main/java/com/example/navigasicompose/ui/view/viewmodel/RencanaStudyViewModel.kt
+++ app/src/main/java/com/example/navigasicompose/ui/view/viewmodel/RencanaStudyViewModel.kt
@@ -1,4 +1,9 @@
 package com.example.navigasicompose.ui.view.viewmodel
 
-class RencanaStudyViewModel {
+import androidx.lifecycle.ViewModel
+import com.example.navigasicompose.model.RencanaStudi
+import kotlinx.coroutines.flow.MutableStateFlow
+
+class RencanaStudyViewModel : ViewModel() {
+    private val _krsState = MutableStateFlow(RencanaStudi())
 }
