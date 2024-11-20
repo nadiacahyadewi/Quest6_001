d0aa60601f97833dda5bde02e2df74d16947f680
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2024-11-20 22:22:17 +0700

    Menambahkan krsStateUi sebagai StateFlow untuk RencanaStudi

--- app/src/main/java/com/example/navigasicompose/ui/view/viewmodel/RencanaStudyViewModel.kt
+++ app/src/main/java/com/example/navigasicompose/ui/view/viewmodel/RencanaStudyViewModel.kt
@@ -3,7 +3,10 @@ package com.example.navigasicompose.ui.view.viewmodel
 import androidx.lifecycle.ViewModel
 import com.example.navigasicompose.model.RencanaStudi
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class RencanaStudyViewModel : ViewModel() {
     private val _krsState = MutableStateFlow(RencanaStudi())
+    val krsStateUi: StateFlow<RencanaStudi> = _krsState.asStateFlow()
 }
