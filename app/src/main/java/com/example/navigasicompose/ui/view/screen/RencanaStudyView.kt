281503f8cf8935cd9a28dd2002900f6ded7ec18f
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2024-11-20 22:15:58 +0700

    menambahkan tombol "Lanjut" yang hanya dapat diklik jika checkbox dalam keadaan tercentang.

--- app/src/main/java/com/example/navigasicompose/ui/view/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/navigasicompose/ui/view/screen/RencanaStudyView.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
@@ -116,6 +117,9 @@ fun RencanaStudiView(
                     Row (
                         modifier = Modifier.fillMaxWidth(),
                     ){
+                        Button(onClick = { onSubmitButtonClicked(listData)}, enabled = checked) {
+                            Text(text = "Lanjut")
+                        }
 
                     }
 
