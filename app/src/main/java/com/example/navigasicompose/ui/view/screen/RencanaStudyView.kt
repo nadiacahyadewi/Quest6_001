aa2d7f5b66a469b1f8f21ebf0124a9d3cfaea994
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2024-11-20 22:03:46 +0700

    Menambahkan Spacer, HorizontalDivider, dan Text dengan teks "Klausul Persetujuan Mahasiswa" dan font tebal

--- app/src/main/java/com/example/navigasicompose/ui/view/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/navigasicompose/ui/view/screen/RencanaStudyView.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -95,6 +96,10 @@ fun RencanaStudiView(
                         fontSize = 12.sp,
                         fontWeight = FontWeight.Light
                     )
+                    Spacer(modifier = Modifier.padding(8.dp))
+                    HorizontalDivider()
+                    Spacer(modifier = Modifier.padding(8.dp))
+                    Text(text = "Klausul Persetujuan Mahasiswa", fontWeight = FontWeight.Bold)
 
                 }
 
