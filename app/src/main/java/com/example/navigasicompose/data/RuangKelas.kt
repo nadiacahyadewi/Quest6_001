3f747038435b052e5956b079caa0b0288e3f39e3
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2024-11-20 21:13:18 +0700

    Initial commit

---
+++ app/src/main/java/com/example/navigasicompose/data/RuangKelas.kt
@@ -0,0 +1,4 @@
+package com.example.navigasicompose.data
+
+object RuangKelas {
+}
---
+++ app/src/main/java/com/example/navigasicompose/model/RencanaStudi.kt
@@ -0,0 +1,6 @@
+package com.example.navigasicompose.model
+
+data class RencanaStudi(
+    val mataKuliah: String = "",
+    val kelas: String = "",
+)
