680cd75e6cac0e54c9d293627cb87ecfea148653
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2024-11-22 14:36:06 +0700

    menambahkan clss MahasiswaViewModel

--- app/src/main/java/com/example/navigasicompose/ui/view/viewmodel/MahasiswaViewModel.kt
+++ app/src/main/java/com/example/navigasicompose/ui/view/viewmodel/MahasiswaViewModel.kt
@@ -1,4 +1,14 @@
 package com.example.navigasicompose.ui.view.viewmodel
 
-class MahasiswaViewModel {
+import androidx.lifecycle.ViewModel
+import com.example.navigasicompose.model.Mahasiswa
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class MahasiswaViewModel : ViewModel() {
+    private val _mahasiswaStateUI = MutableStateFlow(Mahasiswa())
+    val mahasiswaUiState: StateFlow<Mahasiswa> = _mahasiswaStateUI.asStateFlow()
+
+
 }
