2f99d1352d0320770eecef78755ba33e8e6a1be2
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2024-11-22 15:18:07 +0700

    menambahkan fun MahasiswaApp

--- app/src/main/java/com/example/navigasicompose/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/navigasicompose/navigation/PengelolaHalaman.kt
@@ -1,8 +1,24 @@
 package com.example.navigasicompose.navigation
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.navigasicompose.ui.view.viewmodel.MahasiswaViewModel
+import com.example.navigasicompose.ui.view.viewmodel.RencanaStudyViewModel
+
 enum class Halaman {
     Splash,
     Mahasiswa,
     MataKuliah,
     Tampil
 }
+
+@Composable
+fun MahasiswaApp(
+    modifier: Modifier = Modifier,
+    mahasiswaViewModel: MahasiswaViewModel = viewModel(),
+    krsViewModel: RencanaStudyViewModel = viewModel(),
+    navController: NavHostController = rememberNavController()
+){}
