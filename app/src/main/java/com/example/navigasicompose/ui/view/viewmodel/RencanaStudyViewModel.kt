f10c06de90d932d68d415e149271442f0c69e2c8
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2024-11-22 14:01:18 +0700

    menambahkan fun setMataKuliah

--- app/src/main/java/com/example/navigasicompose/ui/view/viewmodel/RencanaStudyViewModel.kt
+++ app/src/main/java/com/example/navigasicompose/ui/view/viewmodel/RencanaStudyViewModel.kt
@@ -5,8 +5,17 @@ import com.example.navigasicompose.model.RencanaStudi
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 class RencanaStudyViewModel : ViewModel() {
     private val _krsState = MutableStateFlow(RencanaStudi())
     val krsStateUi: StateFlow<RencanaStudi> = _krsState.asStateFlow()
+
+    fun setMataKuliah(mkPilihan: String){
+        _krsState.update { stateMK -> stateMK.copy(mataKuliah = mkPilihan) }
+    }
+
+
+
+
 }
